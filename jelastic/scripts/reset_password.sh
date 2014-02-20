e6788fb39faccf880dbbba44ca16cb89301cdaba
Ihor Kolodyuk <ik@jelastic.com>
2014-02-20 13:17:16 +0200

    fix

--- jelastic/scripts/reset_password.sh
+++ jelastic/scripts/reset_password.sh
@@ -10,9 +10,7 @@ SED=$(which sed);
 #$J_OPENSHIFT_APP_ADM_PASSWORD    ;   Use this varible for your password
 
 function _setPassword() {
-    NEO4J_SERVER_PROP_FILE="${CARTRIDGE_HOME}/versions/1.9/neo4j-1.9/conf/neo4j-server.properties";
-    $SED -i "s/^org.neo4j.server.credentials=.*/org.neo4j.server.credentials=${J_OPENSHIFT_APP_ADM_USER}:${J_OPENSHIFT_APP_ADM_PASSWORD}/" $NEO4J_SERVER_PROP_FILE;
-    service cartridge restart;
+	return 0;
 }
 
 
